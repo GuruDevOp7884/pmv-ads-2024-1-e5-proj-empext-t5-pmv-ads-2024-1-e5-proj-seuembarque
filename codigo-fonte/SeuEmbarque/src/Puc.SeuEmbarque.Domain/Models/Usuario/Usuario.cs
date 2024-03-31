aa247160c611d7e6d1833b04cba91cac55474594
Bruno Ferreira <bruno.ferreira@efeso.com>
2024-03-31 10:15:11 -0300

    Nova versao

--- codigo-fonte/SeuEmbarque/src/Puc.SeuEmbarque.Domain/Models/Usuario/Usuario.cs
+++ codigo-fonte/SeuEmbarque/src/Puc.SeuEmbarque.Domain/Models/Usuario/Usuario.cs
@@ -12,5 +12,6 @@ namespace Puc.SeuEmbarque.Domain.Models.Usuario
         public string Nome { get; set; }
         public string Senha { get; set; }
         public string Email { get; set; }
+        public string Telefone { get; set; }
     }
 }
